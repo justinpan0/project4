ece200ec8645d564a2f21040819b32b389011d0d
zimengpan <zimengpan@gmail.com>
2016-10-19 18:22:33 -0400

    Update project4.cpp
    Some changes after CLion running again

--- project4.cpp
+++ project4.cpp
@@ -25,7 +25,7 @@ struct lairInfo{
     string key;
     int alarm;
     string name;
-    string decrypt;
+    string decrypt_key;
     int x;
     int y;
 };
@@ -37,6 +37,7 @@ struct failure{
     int decryption_time;
 };
 // Decryption
+
 string decrypt(string encrypted){
     // Remove all adjacent duplicate characters
     for(int i = 0; i < encrypted.size(); i++){
@@ -83,14 +84,14 @@ int dist(int x1,int y1, int x2, int y2){
 }
 
 int pathFind(int n, int x0, int y0, vector <lairInfo> map){
-    int minDist = dist(x0, y0, map[0].x, map[0][3]);
-    int minID = map[0][1];
+    int minDist = dist(x0, y0, map[0].x, map[0].y);
+    int minID = map[0].ID;
     for(int i=1; i < n; i++){
-        if(minDist > dist(x0, y0, map[i][2], map[i][3])){
-            minDist = dist(x0, y0, map[i][2], map[i][3]);
-            minID = map[i][1];
-        } else if(minDist == dist(x0, y0, map[i][2], map[i][3]) || minID > map[i][1]){
-            minID = map[i][1];
+        if(minDist > dist(x0, y0, map[i].x, map[i].y)){
+            minDist = dist(x0, y0, map[i].x, map[i].y);
+            minID = map[i].ID;
+        } else if(minDist == dist(x0, y0, map[i].x, map[i].y) || minID > map[i].ID){
+            minID = map[i].ID;
         }
     }
     return minID;
@@ -104,15 +105,15 @@ int main(){
     ofstream out_bounty("bounty.txt");
     ofstream out_failure("failure.txt");
 
-    string key, name,decrypt,init_map;
+    string key, name,decrypt_key,init_map;
     int ID, total_money, alarm, num, x_0, y_0, x_f, y_f;
-
+    int x,y;
     // Initialize a vector of struct lairInfo
     vector <lairInfo> info;
     vector <failure> failure;
     int i = 0;
     int p = 0;
-    
+
     in_map >> num >> x_0 >> y_0 >> x_f >> y_f;
     while(in_lair >> ID){
         info[i].ID = ID;
@@ -124,17 +125,17 @@ int main(){
         info[i].alarm = alarm;
         in_lair >> name;
         info[i].name = name;
-        in_rob >> decrypt;
-        info[i].decrypt = decrypt;
+        in_rob >> decrypt_key;
+        info[i].decrypt_key = decrypt_key;
         in_map >> ID >> x >> y;
         info[i].x = x;
         info[i].y = y;
         i++;
     }
-    
-    for(int j=0; j < num; j++){ 
-        string decrypt_key = decrypt(info[j].key);
-        if(decrypt_key != info[j].decrypt){
+
+    for(int j=0; j < num; j++){
+        decrypt_key = decrypt(info[j].key);
+        if(decrypt_key != info[j].decrypt_key){
             failure[p].ID = info[j].ID;
             failure[p].failure_reason = " incorrect decrypted key: ";
             failure[p].incorrect_key = decrypt_key;
@@ -143,14 +144,14 @@ int main(){
         } else if(info[j].key.size() >= info[j].alarm){
             failure[p].ID = info[j].ID;
             failure[p].failure_reason = " decryption tiem too long: ";
-            failure[p].decryption_time = info[j].key.size;
+            failure[p].decryption_time = info[j].key.size();
             info[j].ID = 0;
             j++;
         } else {
             continue;
         }
     }
-    
-    out_failure << info[i].ID << " incorrect decrypted key: " << decrypt_key << endl;
-    out_failure << info.[i].ID << " decryption time too long: " << info[i].key.size();
+
+    //out_failure << info[i].ID << " incorrect decrypted key: " << endl;
+    //out_failure << info.[i].ID << " decryption time too long: " << info[i].key.size();
 }
