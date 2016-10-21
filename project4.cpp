84ebfd60926a8c5a7b26d83d8158999bbb66f596
zimengpan <zimengpan@gmail.com>
2016-10-20 22:59:37 -0400

    Update project4.cpp
    1. The intinerant number is corrected from num to i, so that it wouldnt loop forever 2. the initializaiton of the filedata has to operate each at a time due to the limit of one file open at a time

--- project4.cpp
+++ project4.cpp
@@ -18,7 +18,7 @@
 
 using namespace std;
 
-struct lairInfo{
+struct LairInfo{
     int ID;
     int total_money;
     string key;
@@ -29,14 +29,14 @@ struct lairInfo{
     int y;
 };
 
-struct failure{
+struct Failure{
     int ID;
     string failure_reason;
     string incorrect_key;
     int decryption_time;
 };
-// Decryption
 
+// Decryption
 string decrypt(string encrypted){
     // Remove all adjacent duplicate characters
     for(int i = 0; i < encrypted.size(); i++){
@@ -53,14 +53,14 @@ string decrypt(string encrypted){
         }
     }
 
-    // cout << encrypted << endl;
+    //cout << encrypted << endl;
     // Remove all instances of the first three characters
     string temp = encrypted.substr(0,3);
     while(encrypted.find(temp) != encrypted.npos){
         encrypted.erase(encrypted.find(temp),3);
     }
 
-    // cout << encrypted << endl;
+    //cout << encrypted << endl;
     // If the fist and last two characters match, shift all characters up by 3 in the ACSII map; otherwise, shift by 5
     if(encrypted.substr(0,2) == encrypted.substr(encrypted.size()-2,2)){
         for(int i = 0; i < encrypted.size(); i++){
@@ -82,7 +82,7 @@ int dist(int x1,int y1, int x2, int y2){
     return dist;
 }
 
-int pathFind(int n, int x0, int y0, vector <lairInfo> map){
+int pathFind(int n, int x0, int y0, vector <LairInfo> map){
     int count = 0;
     for(int i = 0; i < n; i++){
         if(map[i].ID != 0){
@@ -111,26 +111,26 @@ int main(){
     ifstream in_lair("lair.txt");
     ifstream in_rob("robbery.txt");
     ifstream in_map("map.txt");
-    ofstream out_bounty("bounty.txt");
-    ofstream out_failure("failure.txt");
+    //ofstream out_bounty("bounty.txt");
+    //ofstream out_failure("failure.txt");
 
     string key, name,decrypt_key,init_map;
     int ID, total_money, alarm, num, x_0, y_0, x_f, y_f;
     int x,y;
     // Initialize a vector of struct lairInfo
-    vector <lairInfo> info;
-    vector <failure> fail;
+    vector <LairInfo> info;
+    vector <Failure> fail;
 
-    fail.push_back(failure());
+    fail.push_back(Failure());
 
     int i = 0;
     int p = 0;
 
     in_map >> num >> x_0 >> y_0 >> x_f >> y_f;
-    cout << num << x_0 << endl;
+    //cout << num << x_0 << endl;
 
     while(in_lair >> ID){
-        info.push_back(lairInfo());
+        info.push_back(LairInfo());
         info[i].ID = ID;
         in_lair >> total_money;
         info[i].total_money = total_money;
@@ -140,7 +140,7 @@ int main(){
         info[i].alarm = alarm;
         in_lair >> name;
         info[i].name = name;
-        // cout << ID << name << endl;
+        //cout << ID << name << endl;
         i++;
     }
 
@@ -153,46 +153,64 @@ int main(){
 
     i = 0;
     while(in_map >> ID){
+        int n=0;
+        while(ID != info[n].ID){
+            n++;
+        }
         in_map >> x >> y;
-        info[i].x = x;
-        info[i].y = y;
-        //cout << x << y << endl;
+        info[n].x = x;
+        info[n].y = y;
+        cout << x << y << endl;
         i++;
     }
-
+    //cout << i << endl;
     // Find the lairs that cannot be robbed
-    for(int j=0; j < num; j++){
-        decrypt_key = decrypt(info[j].key);
-        if(decrypt_key != info[j].decrypt_key){
-            fail.push_back(failure());
+    for(int j=0; j < i; j++){
+        decrypt_key = decrypt(info[j].decrypt_key);
+        //cout << info[j].decrypt_key << "1" << endl;
+        //cout << decrypt(info[j].decrypt_key) << "2" << endl;
+        if(decrypt_key != info[j].key){
+            fail.push_back(Failure());
             fail[p].ID = info[j].ID;
             fail[p].failure_reason = " incorrect decrypted key: ";
             fail[p].incorrect_key = decrypt_key;
             info[j].ID = 0;
             p++;
-            j++;
-        } else if(info[j].key.size() >= info[j].alarm){
-            fail.push_back(failure());
+        } else if(info[j].decrypt_key.size() >= info[j].alarm){
+            fail.push_back(Failure());
             fail[p].ID = info[j].ID;
             fail[p].failure_reason = " decryption tiem too long: ";
-            fail[p].decryption_time = info[j].key.size();
+            fail[p].decryption_time = info[j].decrypt_key.size();
             info[j].ID = 0;
             p++;
-            j++;
-        } else {
-            continue;
         }
     }
-
     // Find the path for all avaiable lairs
+    //cout << 1;
     x = x_0;
     y = y_0;
+    //cout << x << y << endl;
 
-    for(int i = 0; i < num; i++){
-        if(info[i].ID != 0){
+    for(int j = 0; j < i; j++){
+        if(info[j].ID != 0){
             int minID = pathFind(num, x, y, info);
+            int n=0;
+            while(minID != info[n].ID){
+                n++;
+            }
+            cout << info[n].ID << " " << info[n].x << endl;
+            string lair_name = info[n].name;
 
-            out_bounty << "Moved from (" <<
+            while(lair_name.find("_") != lair_name.npos){
+                lair_name.replace(lair_name.find("_"), 1, " ");
+            }
+
+            cout << "Moved from (" << x << "," << y << ") to (" << info[n].x << "," << info[n].y << ")" << endl;
+            cout << "Robbed lair " << lair_name << " and stole $" << info[n].total_money << endl;
+            x = info[n].x;
+            y = info[n].y;
+            n++;
+            info[j].ID = 0;
         }
     }
 
